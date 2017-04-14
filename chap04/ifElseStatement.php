84ebfea2688fc9b059ace38d511f8234d5b34ec6
Shinzo SAITO <architshin@websarva.com>
2017-04-14 15:50:56 +0900

    第4回サンプル追加。

---
+++ chap04/ifElseStatement.php
@@ -0,0 +1,9 @@
+<?php
+$foo = 124;
+if($foo == 123) {
+	print("fooの値は123です<br>");
+}
+else {
+	print("fooの値は123ではありません<br>");
+}
+print("処理終了!");
---
+++ chap04/ifStatement2.php
@@ -0,0 +1,7 @@
+<?php
+$foo = 123;
+$bar = $foo == 123;
+if($bar) {
+	print("fooの値は123です<br>");
+}
+print("処理終了!");
