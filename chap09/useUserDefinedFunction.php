81af591ae6526721394678e4fc02559b5bd1d41f
Shinzo SAITO <architshin@websarva.com>
2017-07-13 16:00:26 +0900

    useUserDefinedFunction.phpの追加。

---
+++ chap09/useUserDefinedFunction.php
@@ -0,0 +1,12 @@
+<?php
+function multiplyArray($array) {
+	$num = 1;
+	foreach($array as $value) {
+		$num *= $value;
+	}
+	return $num;
+}
+
+$list = [5, 4, 8, 6, 2, 9];
+$result = multiplyArray($list);
+print("配列の計算結果: ".$result);
