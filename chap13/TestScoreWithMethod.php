c3c452fd5493128f0f3393cf2c06377639937c2e
Shinzo SAITO <architshin@websarva.com>
2017-11-28 13:32:35 +0900

    Chap13用サンプルの追加。 (#5)
    * Chap13用サンプルの追加。
    * メソッドにpublicを付与。
    * ファイル名を変更し、メソッドを追加。
    * サンプルを2個削除。
    * 戻り値にvoidを記述。

---
+++ chap13/TestScoreWithMethod.php
@@ -0,0 +1,15 @@
+<?php
+class TestScoreWithMethod
+{
+	public $name = "";
+	public $math = 0;
+	public $english = 0;
+	public $japanese = 0;
+
+	public function printScore(): void
+	{
+		$sum = $this->math + $this->english + $this->japanese;
+		$ave = $sum / 3;
+		print($this->name."さんの合計: ".$sum." 平均: ".$ave."<br>");
+	}
+}
