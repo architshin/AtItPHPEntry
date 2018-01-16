c028f19d6e08e6749c494721ba8bc5c35441cd02
Shinzo SAITO <architshin@websarva.com>
2018-01-16 21:04:33 +0900

    サンプル2追加。

--- chap15/StaticMembers.php
+++ chap15/StaticMembers.php
@@ -1,9 +1,9 @@
 <?php
 class StaticMembers
 {
-	//staticプロパティ
+	//staticプロパティ。
 	public static $stNum = 0;
-	//通常のプロパティ
+	//通常のプロパティ。
 	public $num = 0;
 
 	//それぞれのプロパティに引数の数値を加算するメソッド。
@@ -13,5 +13,19 @@ class StaticMembers
 		self::$stNum += $plus;
 		//通常プロパティに加算。
 		$this->num += $plus;
+	}
+
+	//引数と乱数をもとに数値を生成するメソッド。
+	public static function getRandNum(int $init): int
+	{
+		//非staticプロパティに加算。
+		// $this->num += $init;
+		//staticプロパティに引数を加算。
+		self::$stNum += $init;
+		//1～100の乱数を発生。
+		$rand = rand(1, 100);
+		//乱数とプロパティを掛け合わせ、その値を戻り値とする。
+		$ans = $rand * self::$stNum;
+		return $ans;
 	}
 }
