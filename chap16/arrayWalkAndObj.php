04a50402559083128525820f79f573d257515358
Shinzo SAITO <architshin@websarva.com>
2018-03-06 13:03:59 +0900

    ファイル名を変更。

--- chap16/arrayWalkAndObj.php
+++ chap16/arrayWalkAndObj.php
@@ -1,7 +1,7 @@
 <?php
 require_once("InvokeController.php");
 
-$obj = new InvokeController(6);
+$ic = new InvokeController(6);
 
 $list = [1, 3, 5, 7, 9];
-array_walk($list, $obj);
+array_walk($list, $ic);
