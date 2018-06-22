39f1408f38bd730f37080f78298c5159a751dc60
Shinzo SAITO <architshin@websarva.com>
2018-06-22 15:33:00 +0900

    サンプル4追加。

---
+++ chap20/useOrderAdminAuto.php
@@ -0,0 +1,7 @@
+<?php
+require_once("autoload.php");
+
+use atmarkit\phplesson\chap20\classes\businesses\OrderAdminName as OrderAdminBusiness;
+use atmarkit\phplesson\chap20\classes\consumers\OrderAdminName as OrderAdminConsumer;
+
+$orderAdmin = new OrderAdminBusiness();
